f9bb977b419f00c6346f7d57c2dd3bccd795deac
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-12 22:42:33 +0800

    feat: add try except when initializing web drivers

--- automation/web/WebDriverFactory.py
+++ automation/web/WebDriverFactory.py
@@ -1,3 +1,5 @@
+import sys
+
 from selenium import webdriver
 
 from selenium.webdriver.edge.webdriver import WebDriver
@@ -11,41 +13,63 @@ class WebDriverFactory:
         print("Creating instance of WebDriverFactory")
 
     @staticmethod
+    def __get_driver(driver_name: str):
+        driver = None
+        operating_system = platform.system()
+        try:
+            match driver_name:
+                case "CHROME":
+                    driver = webdriver.Chrome()
+                case "FIREFOX":
+                    driver = webdriver.Firefox()
+                case "SAFARI":
+                    if operating_system == 'Darwin':
+                        driver = webdriver.Safari()
+                    else:
+                        print(operating_system + " is an unsupported Operating System to run Safari Web Driver")
+                        exit(1)
+                case "EDGE":
+                    driver = webdriver.Edge()
+                case "IE":
+                    if operating_system == 'Windows':
+                        driver = webdriver.Ie()
+                    else:
+                        print(operating_system + " is an unsupported Operating System to run Internet Explorer Web " +
+                                                 "Driver")
+                        exit(1)
+                case default:
+                    print("Unsupported Web Driver.")
+        except Exception as error_message:
+            print("Encountered Exception when trying to get " + driver_name + " Web Driver: " + str(error_message))
+            exit(1)
+        return driver
+
+    @staticmethod
     def get_chrome_driver():
         print("Initializing Google Chrome Web Driver")
-        chrome_driver: WebDriver = webdriver.Chrome()
+        chrome_driver: WebDriver = WebDriverFactory.__get_driver("CHROME")
         return chrome_driver
 
     @staticmethod
-    def get_firefox_driver(self):
+    def get_firefox_driver():
         print("Initializing Mozilla Firefox Web Driver")
-        firefox_driver: WebDriver = webdriver.Firefox()
+        firefox_driver: WebDriver = WebDriverFactory.__get_driver("FIREFOX")
         return firefox_driver
 
     @staticmethod
-    def get_safari_driver(self):
-        operating_system = platform.system()
-        if operating_system == 'Darwin':
-            print("Initializing Safari Web Driver")
-            safari_driver: WebDriver = webdriver.Safari()
-        else:
-            print("Unsupported Operating System to run Safari Web Driver")
-
+    def get_safari_driver():
+        print("Initializing Safari Web Driver")
+        safari_driver: WebDriver = WebDriverFactory.__get_driver("SAFARI")
         return safari_driver
 
     @staticmethod
-    def get_edge_driver(self):
+    def get_edge_driver():
         print("Initializing Microsoft Edge Web Driver")
-        edge_driver: WebDriver = webdriver.Edge()
+        edge_driver: WebDriver = WebDriverFactory.__get_driver("EDGE")
         return edge_driver
 
     @staticmethod
-    def get_ie_driver(self):
-        operating_system = platform.system()
-        if operating_system == 'Windows':
-            print("Initializing Internet Explorer Web Driver")
-            ie_driver: WebDriver = webdriver.Ie()
-        else:
-            print("Unsupported Operating System to run Internet Explorer Web Driver")
-
+    def get_ie_driver():
+        print("Initializing Internet Explorer Web Driver")
+        ie_driver: WebDriver = WebDriverFactory.__get_driver("IE")
         return ie_driver
