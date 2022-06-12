bbc50bd42fd72c6c5dd90a262c50aae922ab3176
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-13 01:28:08 +0800

    chore: change implementation of static methods

--- automation/web/BrowserCommands.py
+++ automation/web/BrowserCommands.py
@@ -1,4 +1,3 @@
-import self as self
 from selenium import webdriver
 
 
@@ -8,19 +7,18 @@ class BrowserCommands:
         print("Creating instance of BrowserCommands.")
         self.driver: webdriver = driver
 
-    @staticmethod
-    def __execute(task: str, input: str):
+    def __execute(self, task: str, input_value: str):
         print("Performing " + task + " Browser Action.")
         try:
             match str:
                 case "OPEN_TAB":
-                    self.driver.execute_script("window.open('" + input + "', '_blank');");
+                    self.driver.execute_script("window.open('" + input_value + "', '_blank');")
                 case "MAXIMIZE":
                     self.driver.maximize_window()
                 case "DELETE_ALL_COOKIES":
                     self.driver.delete_all_cookies()
                 case "GO_TO":
-                    self.driver.get(input)
+                    self.driver.get(input_value)
                 case "BACK":
                     self.driver.back()
                 case "FORWARD":
