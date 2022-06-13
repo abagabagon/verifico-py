6b60f6d5fbd3b65926e32b3905719e8e2110eb9e
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-13 10:32:57 +0800

    feat: add waits for creating web elements

--- automation/web/WebElementFactory.py
+++ automation/web/WebElementFactory.py
@@ -1,26 +1,25 @@
-from selenium import webdriver
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+
+from automation.web.WaitCommands import WaitCommands
 
 
 class WebElementFactory:
 
-    global local_driver
-
-    def __init__(self, driver: webdriver):
-        self.local_driver = driver
-        print("Creating instance of WebElementFactory")
+    def __init__(self, driver, wait: WaitCommands):
+        self.driver = driver
+        self.wait = wait
+        print("Creating instance of WebElementFactory.")
 
     def create_element(self, locator: By, value: str):
-        element = self.local_driver.find_element(locator, value)
+        element = self.wait.wait_for_element_to_be_present(locator, value)
         return element
 
     def create_element(self, parent_locator: By, parent_value: str, child_locator: By, child_value: str):
-        parent_element = self.local_driver.find_element(parent_locator, parent_value)
-        child_element = parent_element.find_element(child_locator, child_value)
+        child_element = self.wait.wait_for_element_to_be_present(parent_locator, parent_value, child_locator,
+                                                                 child_value)
         return child_element
 
     def create_element(self, parent_element: WebElement, child_locator: By, child_value: str):
-        child_element = parent_element.find_element(child_locator, child_value)
+        child_element = self.wait.wait_for_element_to_be_present(parent_element, child_locator, child_value)
         return child_element
