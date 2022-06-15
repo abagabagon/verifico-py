908d337ee5f1b26627dea571bb3f592cf8ceca6f
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-15 15:07:34 +0800

    chore: apply enums at alert commands

--- automation/web/AlertCommands.py
+++ automation/web/AlertCommands.py
@@ -1,6 +1,15 @@
+from enum import Enum
+
 from selenium import webdriver
 from automation.web.WaitCommands import WaitCommands
 import logging
+from enum import Enum, auto
+
+
+class AlertAction(Enum):
+    ACCEPT = auto()
+    CANCEL = auto()
+    TYPE_INTO = auto()
 
 
 class AlertCommands:
@@ -11,16 +20,16 @@ class AlertCommands:
         self.wait = wait
 
     def accept_alert(self):
-        self.log.debug("Performing ACCEPT Javascript Alert.");
+        self.log.debug("Performing " + str(AlertAction.ACCEPT) + " Javascript Alert.")
         alert = self.wait.wait_for_alert_to_be_present()
         alert.accept()
 
     def cancel_alert(self):
-        self.log.debug("Performing CANCEL Javascript Alert.")
+        self.log.debug("Performing " + str(AlertAction.CANCEL) + " Javascript Alert.")
         alert = self.wait.wait_for_alert_to_be_present()
         alert.dismiss()
 
     def type_alert(self, input_text: str):
-        self.log.debug("Performing TYPE INTO Javascript Alert.");
+        self.log.debug("Performing " + str(AlertAction.TYPE_INTO).replace("_", " ") + " Javascript Alert.")
         alert = self.wait.wait_for_alert_to_be_present()
         alert.send_keys(input_text)
