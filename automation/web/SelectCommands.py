35bfddbfa806f75fa430341ae1a7ee4a46215b8c
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-15 17:06:09 +0800

    fix: removed excess text

--- automation/web/SelectCommands.py
+++ automation/web/SelectCommands.py
@@ -51,7 +51,7 @@ class SelectCommands:
         return action_performed
 
     def __do_command(self, action: SelectAction, locator: By, value: str, input_option: str):
-        log_task = str(action).task.replace("_", " ")
+        log_task = str(action).replace("_", " ")
         self.log.debug(log_task + "ING the option: " + input_option + " from the Web Element " + value + ".")
         for x in range(3):
             element = self.element_factory.create_element(locator, value)
@@ -73,7 +73,7 @@ class SelectCommands:
         self.__do_command(SelectAction.DESELECT, locator, value, input_option)
 
     def __do_command(self, action: SelectAction, parent_locator: By, parent_value: str, child_locator: By, child_value: str, input_option: str):
-        log_task = str(action).task.replace("_", " ")
+        log_task = str(action).replace("_", " ")
         self.log.debug(log_task + "ING the option: " + input_option + " from the Child Web Element " + child_value + " under the Parent Web Element " + parent_value + ".")
         for x in range(3):
             element = self.element_factory.create_element(parent_locator, parent_value, child_locator, child_value)
@@ -95,7 +95,7 @@ class SelectCommands:
         self.__do_command(SelectAction.DESELECT, parent_locator, parent_value, child_locator, child_value, input_option)
 
     def __do_command(self, action: SelectAction, parent_element: WebElement, child_locator: By, child_value: str, input_option: str):
-        log_task = str(action).task.replace("_", " ")
+        log_task = str(action).replace("_", " ")
         self.log.debug(log_task + "ING the option: " + input_option + " from the Child Web Element " + child_value + " under the Parent Web Element " + str(parent_element) + ".")
         for x in range(3):
             element = self.element_factory.create_element(parent_element, child_locator, child_value)
