823a0045fb261a976bed98cb64a2a8914698a35b
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-24 20:22:12 +0800

    feat: reorganize modules and classes

--- com/abagabagon/verifico-py/automation/web/selenium_automation.py
+++ com/abagabagon/verifico-py/automation/web/selenium_automation.py
@@ -1,10 +1,103 @@
-from selenium.common import TimeoutException
+from enum import Enum, auto
+from selenium import webdriver
+from selenium.common import TimeoutException, WebDriverException
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from enum import Enum, auto
+from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager, EdgeChromiumDriverManager
+import platform
+import os
 import logging
+
+
+class BrowserType(Enum):
+    GOOGLE_CHROME = auto()
+    CHROMIUM = auto()
+    BRAVE = auto()
+    MOZILLA_FIREFOX =auto()
+    SAFARI = auto()
+    MICROSOFT_EDGE = auto()
+    INTERNET_EXPLORER = auto()
+
+
+class WebDriverFactory:
+
+    def __init__(self):
+        self.log = logging.getLogger(__name__)
+        self.log.debug("Creating instance of WebDriverFactory.")
+        self.driver = None
+        os.environ['GH_TOKEN'] = "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"
+
+    def __get_driver(self, browser_type: BrowserType):
+        local_task = str(browser_type).replace("_", " ").title()
+        self.log.debug("Initializing " + local_task + " Web Driver")
+        self.driver = None
+        operating_system = platform.system()
+        try:
+            match browser_type:
+                case BrowserType.GOOGLE_CHROME:
+                    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+                case BrowserType.CHROMIUM:
+                    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+                case BrowserType.BRAVE:
+                    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
+                case BrowserType.MOZILLA_FIREFOX:
+                    self. driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+                case BrowserType.SAFARI:
+                    if operating_system == 'Darwin':
+                        self.driver = webdriver.Safari()
+                    else:
+                        self.log.error(operating_system + " is an unsupported Operating System to run SAFARI Web Driver")
+                        exit(1)
+                case BrowserType.MICROSOFT_EDGE:
+                    self.driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+                case BrowserType.INTERNET_EXPLORER:
+                    if operating_system == 'Windows':
+                        self.driver = webdriver.Ie(service=Service(IEDriverManager().install()))
+                    else:
+                        self.log.error(operating_system + " is an unsupported Operating System to run INTERNET EXPLORER Web Driver")
+                        exit(1)
+                case _:
+                    self.log.error(local_task + " is an unsupported Web Driver.")
+        except WebDriverException as error_message:
+            self.log.error("Encountered WebDriverException when trying to get " + local_task + " Web Driver: " + str(error_message))
+        except Exception as error_message:
+            self.log.error("Encountered Exception when trying to get " + local_task + " Web Driver: " + str(error_message))
+            exit(1)
+        return self.driver
+
+    def get_chrome_driver(self):
+        chrome_driver = self.__get_driver(BrowserType.CHROME)
+        return chrome_driver
+
+    def get_chromium_driver(self):
+        chromium_driver = self.__get_driver(BrowserType.CHROMIUM)
+        return chromium_driver
+
+    def get_brave_driver(self):
+        brave_driver = self.__get_driver(BrowserType.BRAVE)
+        return brave_driver
+
+    def get_firefox_driver(self):
+        firefox_driver = self.__get_driver(BrowserType.FIREFOX)
+        return firefox_driver
+
+    def get_safari_driver(self):
+        safari_driver = self.__get_driver(BrowserType.SAFARI)
+        return safari_driver
+
+    def get_edge_driver(self):
+        edge_driver = self.__get_driver(BrowserType.EDGE)
+        return edge_driver
+
+    def get_ie_driver(self):
+        ie_driver = self.__get_driver(BrowserType.IE)
+        return ie_driver
 
 
 class WaitAction(Enum):
@@ -306,3 +399,24 @@ class WaitCommands:
         except Exception as error_message:
             self.log.error("Encountered Exception when trying to wait for Javascript " + str(WaitAction.ALERT_TO_BE_PRESENT).replace("_", " ").title() + ": " + str(error_message))
         return alert
+
+
+class WebElementFactory:
+
+    def __init__(self, driver, wait: WaitCommands):
+        self.log = logging.getLogger(__name__)
+        self.log.debug("Creating instance of WebElementFactory.")
+        self.driver = driver
+        self.wait = wait
+
+    def create_element(self, locator: By, value: str):
+        element = self.wait.wait_for_element_to_be_present(locator, value)
+        return element
+
+    def create_element(self, parent_locator: By, parent_value: str, child_locator: By, child_value: str):
+        child_element = self.wait.wait_for_element_to_be_present(parent_locator, parent_value, child_locator, child_value)
+        return child_element
+
+    def create_element(self, parent_element: WebElement, child_locator: By, child_value: str):
+        child_element = self.wait.wait_for_element_to_be_present(parent_element, child_locator, child_value)
+        return child_element
